68cbecee0322ab7c554728fc86df0ff060113f04
Ashen-Ashiok <theyareashiok@gmail.com>
2020-11-18 14:47:17 +0100

    Project name change, adding functions

--- whisperer.py
+++ whisperer.py
@@ -14,13 +14,13 @@ from tqdm import tqdm
 
 parser = argparse.ArgumentParser()
 parser.add_argument("-i", "--url-list", help="List of URL/filename pairs, delimited by tabs")
-parser.add_argument("--pre-download", help="Pre-download the video and audio files, mix after", action="store_true")
 parser.add_argument("--no-mix", help="Do not merge audio and video files", action="store_true")
 parser.add_argument("--no-delete", help="Do not merge audio and video files", action="store_true")
 args = parser.parse_args()
 
-local_list = []
-if args.pre_download:
+
+def download_files():
+    files_map = []
     for line in open(args.url_list):
         output_file, master_json_url = line.rstrip().split('\t')
         print('\n\n\nProcessing %s' % output_file)
@@ -37,10 +37,10 @@ if args.pre_download:
         video_base_url = base_url + video["base_url"]
         print("Base url:", video_base_url)
 
-        filenameVideo = "video_%s.mp4" % output_file
-        print("Saving VIDEO to %s" % filenameVideo)
+        video_filename = "video_%s.mp4" % output_file
+        print("Saving VIDEO to %s" % video_filename)
 
-        video_file = open(filenameVideo, "wb")
+        video_file = open(video_filename, "wb")
 
         init_segment = base64.b64decode(video["init_segment"])
         video_file.write(init_segment)
@@ -69,10 +69,10 @@ if args.pre_download:
         audio_base_url = base_url + audio["base_url"]
         print("Base url:", audio_base_url)
 
-        filenameAudio = "audio_%s.mp4" % output_file
-        print("Saving AUDIO to %s" % filenameAudio)
+        audio_filename = "audio_%s.mp4" % output_file
+        print("Saving AUDIO to %s" % audio_filename)
 
-        audio_file = open(filenameAudio, "wb")
+        audio_file = open(audio_filename, "wb")
 
         init_segment = base64.b64decode(audio["init_segment"])
         audio_file.write(init_segment)
@@ -93,22 +93,31 @@ if args.pre_download:
         audio_file.close()
         print("Finished downloading video and audio for file %s" % output_file)
 
-        local_list.append((output_file, filenameAudio, filenameVideo))
+        files_map.append((output_file, audio_filename, video_filename))
 
-if not args.no_mix:
-    for output_file, filenameAudio, filenameVideo in local_list:
+    return files_map
+
+
+def mix_files(files_map):
+    for output_filename, audio_filename, video_filename in files_map:
         # Combine audio and video here
-        print("Combining video and audio for file %s" % output_file)
+        print("Mixing video and audio for file %s" % output_filename)
         cmd = "ffmpeg -y -i "
-        cmd += filenameAudio
+        cmd += audio_filename
         cmd += " -i "
-        cmd += filenameVideo
-        cmd += " " + output_file
+        cmd += video_filename
+        cmd += " " + output_filename
         subprocess.call(cmd, shell=True)
-        print("Mixing for file %s done" % output_file)
+        print("Mixing for file %s done" % output_filename)
 
         if not args.no_delete:
             # Delete the remaining single audio and video files
-            os.remove(filenameAudio)
-            os.remove(filenameVideo)
-            print("Temporary files {} and {} removed!".format(filenameAudio, filenameVideo))
+            os.remove(audio_filename)
+            os.remove(video_filename)
+            print("Temporary files {} and {} removed!".format(audio_filename, video_filename))
+
+
+audio_video_output_map = download_files()
+
+if not args.no_mix:
+    mix_files(audio_video_output_map)
